8e3b56e82db8ca2309519c51cf6ffdaf26ff0191
dartbill <billieldartnell@hotmail.com>
2022-07-06 14:30:27 +0100

    change set up

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, jsonify
-from flask_pymongo import pymongo
-
+from flask import Flask
+from flask_cors import CORS
+import pymongo
+  
+# Replace your URL here. Don't forget to replace the password.
+connection_url = 'mongodb+srv://billiedartnell:Orangelemon19@cluster0.0z2m7lp.mongodb.net/?retryWrites=true&w=majority'
 app = Flask(__name__)
-# app.config["MONGO_URI"] = "mongodb+srv://billiedartnell:Orangelemon19@cluster0.0z2m7lp.mongodb.net/?retryWrites=true&w=majority"
-# mongo = PyMongo(app)
+client = pymongo.MongoClient(connection_url)
+  
+# Database
+Database = client.get_database('sample_restaurants')
+# Table
+SampleTable = Database.sample_restaurants
 
 
-CONNECTION_STRING = "mongodb+srv://test:test@flask-mongodb-atlas-1g8po.mongodb.net/test?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('flask_mongodb_atlas')
-user_collection = pymongo.collection.Collection(db, 'sample_restaurants')
+@app.route('/')
+def hello():
+    return jsonify({"hello": "world"}),200
 
-
-@app.route("/")
-def home_page():
-    online_users = db.db.collection.restaurants.find({"borough": 'Brooklyn'})
-    return online_users
+if __name__ == '__main__':
+    app.run(debug=True)
