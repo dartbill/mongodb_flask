54e8a2b047a40d97fe7f7479a2eb9244f1228e3d
dartbill <billieldartnell@hotmail.com>
2022-07-06 13:59:09 +0100

    change url

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://billiedartnell:Orangelemon19@cluster0.0z2m7lp.mongodb.net/?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://billiedartnell:OrangeLemon19@cluster0.0z2m7lp.mongodb.net/?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 @app.route("/")
