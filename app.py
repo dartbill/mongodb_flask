ace53dbc7c27dac2f7c800b35ef43ae11955e144
dartbill <billieldartnell@hotmail.com>
2022-07-06 14:18:57 +0100

    test

--- app.py
+++ app.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://billiedartnell:Orangelemon19@cluster0.0z2m7lp.mongodb.net/?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
+
+CONNECTION_STRING = "mongodb+srv://test:test@flask-mongodb-atlas-1g8po.mongodb.net/test?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('flask_mongodb_atlas')
+user_collection = pymongo.collection.Collection(db, 'sample_restaurants')
+
+
 @app.route("/")
 def home_page():
-    online_users = mongo.db.sample_restaurants.restaurants.find({"borough": 'Brooklyn'})
+    online_users = db.db.collection.restaurants.find({"borough": 'Brooklyn'})
     return online_users
