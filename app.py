9cf91221ff1c3cfe5b30a9c5e03f8c54fcbdc448
dartbill <billieldartnell@hotmail.com>
2022-07-07 21:19:59 +0100

    try methods

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pymongo
-  
+
 # Replace your URL here. Don't forget to replace the password.
 connection_url = 'mongodb+srv://billiedartnell:<password>@cluster0.0z2m7lp.mongodb.net/?retryWrites=true&w=majority'
 app = Flask(__name__)
 client = pymongo.MongoClient(connection_url)
-  
+
 # Database
 Database = client.get_database('example')
 # Table
@@ -15,19 +15,24 @@ SampleTable = Database.sample
 
 @app.route('/')
 def hello():
-    return jsonify({"hello": "world"}),200
+    return jsonify({"hello": "world"}), 200
 
 
-@app.route('/db')
+@app.route('/db', methods=["GET", "POST"])
 def get_db():
-    query = SampleTable.find()
-    output = {}
-    i = 0
-    for x in query:
-        output[i] = x
-        output[i].pop('_id')
-        i += 1
-    return jsonify(output)
+    if request.method == 'GET':
+
+        query = SampleTable.find()
+        output = {}
+        i = 0
+        for x in query:
+            output[i] = x
+            output[i].pop('_id')
+            i += 1
+        return jsonify(output)
+    elif request.method == 'POST':
+        return jsonify('message: hello')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
