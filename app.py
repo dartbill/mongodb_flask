ff446985c4b8b1d63247072653cab93be549d49b
dartbill <billieldartnell@hotmail.com>
2022-07-08 11:18:28 +0100

    get by name now working

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import json
 from random import sample
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from bson.json_util import dumps
 import pymongo
 
 # Replace your URL here. Don't forget to replace the password.
@@ -35,18 +37,24 @@ def get_db():
 @app.route('/db/<name>', methods=["GET"])
 def get_db_by_name(name):
     query = SampleTable.find_one({"name": name})
-    output = query
+
+    output = {'name': query['name'], 'age': query['age']}
+    # i = 0
+    # for x in query:
+    #     output[i] = x
+    #     # output[i].pop('_id')
+    #     i += 1
     return jsonify(output)
 
 
-@app.route('/db', methods=["POST"])
+@ app.route('/db', methods=["POST"])
 def post_db():
     new_user = request.get_json()
     SampleTable.insert_one(new_user)
     return jsonify('a new user has been added')
 
 
-@app.route('/db/:<id>', methods=["PATCH"])
+@ app.route('/db/:<id>', methods=["PATCH"])
 def update_db():
     update_user = SampleTable.find_one({"name": id})
     data = request.get_json()
